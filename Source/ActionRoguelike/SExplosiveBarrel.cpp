be9dcef6a26bec99a05e2ef10044bf65cf2db2af
Mark Agne <magnenator@gmail.com>
2024-02-19 17:34:13 -0600

    explosive barrel C++ class and blueprint, receives OnHit events

---
+++ Source/ActionRoguelike/SExplosiveBarrel.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SExplosiveBarrel.h"
+
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	Mesh->SetupAttachment(RootComponent);
+	
+	Mesh->SetCollisionProfileName("Destructible");
+}
+
+// Called when the game starts or when spawned
+void ASExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Mesh->OnComponentHit.AddDynamic(this, &ASExplosiveBarrel::OnHit);
+}
+
+// Called every frame
+void ASExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ASExplosiveBarrel::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                              FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Hit Barrel."));
+}
