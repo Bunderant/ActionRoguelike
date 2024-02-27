74446b1bb4f997e434bb8b860b8a3b3b50d0cedd
Mark Agne <magnenator@gmail.com>
2024-02-27 16:59:33 -0600

    new "target" BP & C++ class to demo "hit flash"

---
+++ Source/ActionRoguelike/Private/STargetDummy.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "STargetDummy.h"
+
+#include "SAttributeComponent.h"
+
+
+// Sets default values
+ASTargetDummy::ASTargetDummy()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("Mesh Component");
+	RootComponent = MeshComponent;
+
+	HealthAttributeComponent = CreateDefaultSubobject<USAttributeComponent>("Health Attribute");
+}
+
+void ASTargetDummy::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	HealthAttributeComponent->OnAttributeChanged.AddDynamic(this, &ASTargetDummy::HandleHealthChanged);
+}
+
+void ASTargetDummy::HandleHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComponent, float Value,
+	float Delta)
+{
+	if (Delta >= 0)
+	{
+		return;
+	}
+
+	MeshComponent->SetScalarParameterValueOnMaterials("TimeOfHit", GetWorld()->TimeSeconds);
+}
+
---
+++ Source/ActionRoguelike/Public/STargetDummy.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STargetDummy.generated.h"
+
+class USAttributeComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASTargetDummy : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASTargetDummy();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> MeshComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USAttributeComponent> HealthAttributeComponent;
+	
+	// Called when the game starts or when spawned
+	virtual void PostInitializeComponents() override;
+
+	UFUNCTION()
+	void HandleHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComponent, float Value, float Delta);
+};
