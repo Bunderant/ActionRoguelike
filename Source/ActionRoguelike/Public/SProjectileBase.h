bf7b89b584fd9d790efe00bfd143a948b0463111
Mark Agne <magnenator@gmail.com>
2024-02-27 20:39:22 -0600

    removed redundant projectile base "implementation" declarations
    These virtual declarations are implicitly created by the BlueprintNativeEvent property specifier.

--- Source/ActionRoguelike/Public/SProjectileBase.h
+++ Source/ActionRoguelike/Public/SProjectileBase.h
@@ -42,11 +42,9 @@ protected:
 
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, meta=(DisplayName="Handle Projectile Overlap"))
 	void HandleProjectileOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-	virtual void HandleProjectileOverlap_Implementation(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, meta=(DisplayName="Handle Projectile Hit"))
 	void HandleProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
-	virtual void HandleProjectileHit_Implementation(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 public:
 	// Called every frame
