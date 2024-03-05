5ad63354373dfe0d6c26a688c3c28c510bbe8606
Mark Agne <magnenator@gmail.com>
2024-03-04 18:55:09 -0600

    custom C++ behavior tree service to check attack range, integrated into minion tree

---
+++ Source/ActionRoguelike/Private/AI/SBTService_CheckAttackRange.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTService_CheckAttackRange.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void USBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	UBlackboardComponent* BlackboardComponent = OwnerComp.GetBlackboardComponent();
+	if (!ensure(BlackboardComponent)) return;
+
+	AActor* TargetActor = Cast<AActor>(BlackboardComponent->GetValueAsObject(TargetActorKey.SelectedKeyName));
+	if (!ensure(TargetActor)) return;
+
+	AAIController* MyController = OwnerComp.GetAIOwner();
+	if (!ensure(MyController)) return;
+
+	APawn* MyPawn = MyController->GetPawn();
+	if (!ensure(MyPawn)) return;
+
+	float DistanceTo = TargetActor->GetDistanceTo(MyPawn);
+	bool bIsWithinRange = DistanceTo <= 1000.0f;
+
+	BlackboardComponent->SetValueAsBool(IsWithinRangeKey.SelectedKeyName, bIsWithinRange);
+}
