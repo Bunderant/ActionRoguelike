9248b264b5529714ddd738a2001fc8736127aad3
Mark Agne <magnenator@gmail.com>
2024-04-10 21:19:24 -0500

    "CommonGame" plugin from Lyra + dependencies
    This is the foundation needed for accessing a UI subsystem enabling us to push/pop menus within "layers" defined by gameplay tags. ModularGameplayActors is a somewhat odd requirement, but it's just a lightweight plugin containing abstract classes that provide consistent/easy hooks for tapping into the ModularGameplay plugin's functionality.
    
    Could have rewritten the UI functionality myself, but figured this was a good time to get up to speed with the "Lyra" way of doing things, and getting acquainted with modular gameplay features soon can only be a good thing for prototyping.

--- Source/ActionRoguelike/ActionRoguelike.Build.cs
+++ Source/ActionRoguelike/ActionRoguelike.Build.cs
@@ -19,10 +19,16 @@ public class ActionRoguelike : ModuleRules
 			"GameplayTasks",
 			"CommonUI",
 			"UMG",
-			"GameplayTags"
+			"GameplayTags",
+			"ModularGameplay",
+			"ModularGameplayActors"
 		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"CommonGame",
+			"CommonUser"
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
