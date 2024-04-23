063922b9bed49825c9f328ebfb944468d2ec9b9b
Mark Agne <magnenator@gmail.com>
2024-04-23 17:59:33 -0500

    "stunned" effect, with accompanying animation (on Gideon)

---
+++ Source/ActionRoguelike/Private/Animation/SAnimInstance.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Animation/SAnimInstance.h"
+
+#include "Actions/SActionComponent.h"
+
+void USAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (const AActor* OwningActor = GetOwningActor())
+	{
+		ActionComp = OwningActor->FindComponentByClass<USActionComponent>();
+	}
+}
+
+void USAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (ActionComp)
+	{
+		const FGameplayTag StunnedTag = FGameplayTag::RequestGameplayTag("Status.Stunned");
+		bIsStunned = ActionComp->ActiveGameplayTags.HasTag(StunnedTag);
+	}
+}
